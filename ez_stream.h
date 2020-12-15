7580aee6348b64d648cd75a850c9263d2a0eafd6
detolly <sparklez@detolly.no>
2020-12-15 18:03:18 +0100

    Quality of life change

--- ez_stream.h
+++ ez_stream.h
@@ -7,11 +7,12 @@ public:
 	void seek(int index) { m_index = index; }
 	[[nodiscard]] const inline unsigned int tell() { return m_index; }
 
-	template <typename T, size_t size = sizeof(T)> const inline T read() {
+	template <typename T, size_t size = sizeof(T)>
+	[[nodiscard]] const inline T read() {
 		T ret = *(T*)((uint32_t)m_buffer + m_index);
 		m_index += size;
-		if (m_index > m_len)
-			throw std::exception("ur doing it wrong");
+		//if (m_index > m_len)
+		//	throw std::exception("ur doing it wrong");
 		return ret;
 	}
 
