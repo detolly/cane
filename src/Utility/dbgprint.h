dad64df0ee22ba41158807dda479f2a0c39b4ddd
Thomas <sparklez@detolly.no>
2021-02-18 22:02:57 +0100

    sink

--- src/Utility/dbgprint.h
+++ src/Utility/dbgprint.h
@@ -2,13 +2,18 @@
 
 #include <iostream>
 
+namespace {
+template<typename ...T> void sink(const T& ...) {}
+}
+
 template <typename... T>
-inline static void dbgprint(const char* a, T ...args) {
+inline static void dbgprint(const char* a, const T& ...args) {
 #ifndef NDEBUG
 	printf(a, args...);
 #endif
 #ifdef NDEBUG
     (void)(a);
+    sink(args...);
 #endif
 }
 
